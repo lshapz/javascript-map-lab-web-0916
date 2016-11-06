2269e2a3aba6fd3681b40a497cb398890659dbbf
lshapz <laurashapiro01@gmail.com>
2016-11-06 12:48:29 -0500

    other people's answers for future reference

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ var sortedRobots = robots.map(function(robot) {
   })
 })
 
+// var sortedRobots = robots.map(function(robot){
+//   if( knownDecepticons.indexOf(robot.name) > -1 ){
+//     return Object.assign({}, robot,{ alliance: "decepticon"})
+//   }else {
+//     return Object.assign({}, robot, { alliance: "autobot"})
+//   }
+// })
+
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -35,9 +44,17 @@ var coloredZebraStripes = zebraStripes.map(function(zeb, index)
   const even = (index % 2) === 0; 
   return Object.assign({}, zeb, {
     color: even ? 'black' : 'white'
-
   })
-
-
-
 })
+
+
+// var coloredZebraStripes = zebraStripes.map(function (bra, index) {
+//     let ze = Object.assign({}, bra)
+
+//     if (index % 2 === 0) {
+//       ze.color = "black"
+//     } else {
+//       ze.color = "white"
+//     }
+//   return ze
+// });
